fc42ecb40f862c0b34eed7e3e8284bd9864aa5cd
shawn <shawn@gmail.com>
2025-06-06 16:37:52 +0800

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html'
+		}),
 		paths: {
 			base: process.env.BASE_PATH || '/yonkuro',
 			relative: true
+		},
+		prerender: {
+			handleHttpError: ({ status, path, referrer, referenceType }) => {
+				// 只要不是 200，都忽略，不要 throw
+				if (status === 404) return;
+				throw new Error(`${status} ${path}`);
+			}
 		}
 	}
 };
