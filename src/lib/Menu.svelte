cd1b0d1c29e434b860c420b4befca299f6f57bb4
shawn <shawn@gmail.com>
2025-06-09 00:35:18 +0800

    add menu component

---
+++ src/lib/Menu.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, DarkMode, Span } from 'flowbite-svelte';
+	import * as Icon from 'flowbite-svelte-icons';
+	import { base } from '$app/paths';
+
+	const menues = [
+		{ href: `${base}/`, label: 'Home' },
+		{ href: `${base}/about`, label: 'About' },
+		{ href: `${base}/contact`, label: 'Contact' }
+	];
+</script>
+
+<Navbar class="left-0 w-full px-2 py-0 dark:bg-gray-800" color="form">
+	<NavBrand href="/">
+		<img src="https://svelte.dev/favicon.png" class="mr-3 h-6 sm:h-9" alt="Svelte Logo" />
+		<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+			選擇障礙
+		</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl>
+		{#each menues as { href, label }}
+			<NavLi {href}>{label}</NavLi>
+		{/each}
+	</NavUl>
+	<DarkMode class="text-lg">
+		{#snippet lightIcon()}
+			<Icon.ThumbsUpSolid color="white" />
+		{/snippet}
+		{#snippet darkIcon()}
+			<Icon.ThumbsDownSolid color="green" />
+		{/snippet}
+	</DarkMode>
+</Navbar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
 	import '../app.css';
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
-	import { base } from '$app/paths';
+	import Menu from '$lib/Menu.svelte';
+	import { Span } from 'flowbite-svelte';
 
 	let { children } = $props();
 </script>
 
-<div class="w-full left-0 fixed z-40 block">
-	<Navbar class="px-2 py-1 w-full left-0 border-b flex flex-wrap justify-between items-center">
-	  <NavBrand href="{base}/">
-		<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-	  </NavBrand>
-	  <NavHamburger />
-	  <NavUl>
-		<NavLi href="{base}/">Home</NavLi>
-		<NavLi href="{base}/about">About</NavLi>
-	  </NavUl>
-	</Navbar>
+<div class="relative">
+	<Menu />
 	{@render children()}
+	<Span class="fixed right-3 bottom-1">{import.meta.env.VITE_VERSION}</Span>
 </div>
