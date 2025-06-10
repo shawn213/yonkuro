ec59a3670ee6fa2f59061cb9ae036fe5f93b074b
shawn <shawn@gmail.com>
2025-06-10 13:41:15 +0800

    add qr code url

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import QR from '@svelte-put/qr/svg/QR.svelte';
+	import { page } from '$app/state';
+</script>
+
+<div class="m-5 h-96 w-96 bg-white">
+	<QR
+		data={page.url.origin + page.url.pathname}
+		logo="https://svelte.dev/favicon.png"
+		moduleFill="green"
+		anchorOuterFill="green"
+		anchorInnerFill="green"
+		shape="circle" />
+</div>
