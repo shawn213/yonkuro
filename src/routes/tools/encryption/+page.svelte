3cdfb1e1cc6233a07aceaabe1a2102f61f79df03
shawn <shawn@gmail.com>
2025-06-19 17:31:32 +0800

    加入加解密功能

--- src/routes/tools/encryption/+page.svelte
+++ src/routes/tools/encryption/+page.svelte
@@ -1,70 +1,135 @@
-<script>
+<script lang="ts">
 	import { Button, Input, Label, Select, Textarea, Toggle } from 'flowbite-svelte';
 	import { Base64 } from 'js-base64';
+	import { jwtDecode } from 'jwt-decode';
 	import json5 from 'json5';
+	import Rabbit from 'crypto-js/rabbit';
+	import encUtf8 from 'crypto-js/enc-utf8';
 
-	let body = '';
-	let target = '';
-	let regex = '';
+	const rows: number = 20;
+	const encrypts = [
+		{ value: 'default', name: 'default' },
+		{ value: 'base64', name: 'base64' },
+		{ value: 'jwt', name: 'jwt' },
+		{ value: 'sha1', name: 'sha1' }
+	];
+	let toggle: boolean = true;
+	let content: string = '';
+	let target: string = '';
+	let key: string = '';
+	let selected: string = 'default';
 
-	const encodingText = (text) => Base64.encode(text);
-	const encodeBody = (body, path = '', encodedPaths = null) => {
-		const isObject = (data) => data && typeof data === 'object' && !Array.isArray(data);
+	let buttonText: string = '解密';
+	let contentText = '密文';
+	let contentPlaceholder = '例: U2FsdGVkX18cbHi2CxrSUnAEhwx+0g==';
+	let disabled = false;
+	let keyShow = true;
+	let inputShow = true;
+	$: switch (selected) {
+		case 'jwt':
+			inputShow = true;
+			toggle = false;
+			disabled = true;
+			keyShow = false;
+			break;
+		case 'sha1':
+			inputShow = false;
+			toggle = true;
+			disabled = true;
+			keyShow = false;
+			break;
+		case 'base64':
+			inputShow = true;
+			disabled = false;
+			keyShow = false;
+			break;
+		default:
+			inputShow = true;
+			disabled = false;
+			keyShow = true;
+			break;
+	}
+	$: if (toggle) {
+		buttonText = 'Decryption';
+		contentText = 'Ciphertext';
+		contentPlaceholder = '例: U2FsdGVkX18cbHi2CxrSUnAEhwx+0g==';
+	} else {
+		buttonText = 'Encryption';
+		contentText = 'Original';
+		contentPlaceholder = 'Content';
+	}
 
-		if (encodedPaths === null) {
-			encodedPaths = [];
-		}
+	const byteToHex = (byte) => {
+		return ('0' + byte.toString(16)).slice(-2);
+	};
 
-		// 判斷 value 是否包含這些字串的正規表達式
-		const valuePattern = new RegExp(regex);
+	const generateId = (len = 40) => {
+		var arr = new Uint8Array(len / 2);
+		window.crypto.getRandomValues(arr);
+		return Array.from(arr, byteToHex).join('');
+	};
 
-		for (const key in body) {
-			if (Object.prototype.hasOwnProperty.call(body, key)) {
-				const value = body[key];
-				const currentPath = path ? `${path}.${key}` : key;
-
-				if (typeof value === 'string' && valuePattern.test(value)) {
-					body[key] = encodingText(value);
-					encodedPaths.push(currentPath);
-				} else if (
-					Array.isArray(value) &&
-					value.some((item) => typeof item === 'string' && valuePattern.test(item))
-				) {
-					body[key] = value.map((item) =>
-						typeof item === 'string' && valuePattern.test(item) ? encodingText(item) : item
-					);
-					encodedPaths.push(currentPath);
-				} else if (isObject(value)) {
-					encodeBody(value, currentPath, encodedPaths);
-				} else if (Array.isArray(value)) {
-					body[key] = value.map((item, index) => {
-						if (isObject(item)) {
-							return encodeBody(item, `${currentPath}[${index}]`, encodedPaths);
-						} else {
-							return item;
-						}
-					});
-				}
+	const handleClick = () => {
+		if (toggle) {
+			switch (selected) {
+				case 'base64':
+					target = Base64.atob(content);
+					break;
+				case 'jwt':
+					target = JSON.stringify(jwtDecode(content), null, '\t');
+					break;
+				case 'sha1':
+					target = generateId();
+					break;
+				default:
+					const decrypted = Rabbit.decrypt(content, key);
+					target = decrypted.toString(encUtf8);
+					break;
+			}
+		} else {
+			switch (selected) {
+				case 'base64':
+					target = Base64.btoa(content);
+					break;
+				case 'jwt':
+					target = JSON.stringify(jwtDecode(content), null, '\t');
+					break;
+				case 'sha1':
+					target = generateId();
+					break;
+				default:
+					target = Rabbit.encrypt(content, key).toString();
+					break;
 			}
 		}
-
-		if (path === '') {
-			body.$decode = encodedPaths;
-		}
-
-		return body;
+	};
+	const handleClickToggle = () => {
+		[content, target] = [target, content];
 	};
 </script>
 
 <div class="grid w-full grid-cols-2 items-center gap-2">
-	<Textarea placeholder="請輸入專案代碼（小寫）" bind:value={body} rows={40} />
-	<Textarea placeholder="請輸入專案代碼（小寫）" bind:value={target} rows={40} />
+	<div class="flex items-center">
+		<Label for="toggle" class="mr-2 text-right">Switch</Label>
+		<Toggle
+			bind:checked={toggle}
+			{disabled}
+			color={disabled ? 'gray' : 'green'}
+			onclick={handleClickToggle} />
+		<Label for="encoding" class="mr-2 text-right">Encryption</Label>
+		<Select id="encoding" bind:value={selected} class="w-full max-w-md" items={encrypts} />
+	</div>
+	<Label for="encoding" class="text-right">.</Label>
+	<Label for="source">{contentText}</Label>
+	<Label for="target">Result</Label>
+	<Textarea placeholder="任意格式非加密文" bind:value={content} {rows} />
+	<Textarea placeholder="密文" bind:value={target} {rows} />
 </div>
+{#if inputShow}
+	<div class="mb-2 flex justify-center">
+		<Input type="text" bind:value={key} class="w-full max-w-md" />
+	</div>
+{/if}
 <div class="mb-2 flex justify-center">
-	<Input type="text" bind:value={regex} class="w-full max-w-md" />
-</div>
-<div class="mb-2 flex justify-center">
-	<Button onclick={() => (target = JSON.stringify(encodeBody(json5.parse(body)), null, '\t'))}>
-		測試
-	</Button>
+	<Button onclick={handleClick}>{buttonText}</Button>
 </div>
