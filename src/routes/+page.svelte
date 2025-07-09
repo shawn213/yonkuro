e5526213a9288f787bd638cfd7188cdfcb68545d
shawn <shawn@gmail.com>
2025-07-09 17:52:43 +0800

    調整 layout 大小

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,12 @@
 	import { page } from '$app/state';
 </script>
 
-<div class="flex h-screen w-screen items-center justify-center md:items-start md:justify-start">
-	<div class="h-80 w-80 bg-white md:h-96 md:w-96">
-		<QR
-			data={page.url.origin + page.url.pathname}
-			logo="https://svelte.dev/favicon.png"
-			moduleFill="green"
-			anchorOuterFill="green"
-			anchorInnerFill="green"
-			shape="circle" />
-	</div>
+<div class="h-80 w-80 bg-white md:h-96 md:w-96">
+	<QR
+		data={page.url.origin + page.url.pathname}
+		logo="https://svelte.dev/favicon.png"
+		moduleFill="green"
+		anchorOuterFill="green"
+		anchorInnerFill="green"
+		shape="circle" />
 </div>
